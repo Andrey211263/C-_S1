f20f1bac3ac3ff51109f98a7cb3d7e81f30c9163
Andrey <a_mikhaylov@inbox.ru>
2022-08-20 19:38:24 +0300

    rename cwd

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Enter number a: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter number b: ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+
+if(numbera > numberb)
+ {
+ Console.Write( "max = "+numbera );
+ Console.WriteLine( ": min = "+numberb);
+ }
+ else
+ {
+ Console.Write( "max = "+numberb );
+Console.WriteLine( ": min = "+numbera);
+ }
---
+++ Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿System.Console.Write("Enter number a: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter number b: ");
+int numberb = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter number c: ");
+int numberc = Convert.ToInt32(Console.ReadLine());
+int max = numbera;
+int min = numbera;
+
+if( numbera > max )
+    {
+            max = numbera;
+    }
+    else
+    if( numbera < min ) min = numbera;
+    
+if( numberb > max )
+     {
+            max = numberb;
+     }
+     else
+     if( numberb < min ) min = numberb;
+   
+if( numberc > max )
+    {
+            max = numberc;
+    }
+    else 
+    if( numbera < min ) min = numberc;
+    
+ Console.WriteLine( "max = "+ max+": min = "+ min);
+ 
