50f62d44a3cf7a48b06587bb6f46fca0c0378e92
Andrey <a_mikhaylov@inbox.ru>
2022-08-20 20:02:38 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// организован цикл на ввод 3-х чисел с анализо чет/нечет
+int count = 0;
+while(count < 3 )
+{
+System.Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0)
+{
+    System.Console.WriteLine(number+": четное ");
+}
+else
+{
+System.Console.WriteLine(number+": не четное ");
+}
+count++;
+}
