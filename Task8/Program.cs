c78ca02b11c1a4f76e5e3244d29a6c2444d78769
Andrey <a_mikhaylov@inbox.ru>
2022-08-20 21:00:38 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывод четных от 1 до n
+System.Console.Write("Enter number a: ");
+int numbera = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+Console.Write( "Четные числа:" );
+
+if( numbera % 2 != 0)
+ {
+    numbera = numbera - 1;
+ }
+ {
+while(count < numbera)
+{
+    count +=2;
+    Console.Write( " , "+count );
+}
+ }
+ 
